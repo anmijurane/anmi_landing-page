8921a2f3e73c97721aeb3040f78129d0fffd2a89
M. Andres Jurado <anmijurane@gmail.com>
2025-11-22 00:44:16 -0600

    BUGFIX: sync navbar responsive design

--- src/scripts/scrollSpyNav.ts
+++ src/scripts/scrollSpyNav.ts
@@ -1,27 +1,35 @@
-import { _$, _$All } from "./querys";
+import { cspAlgorithmSchema } from 'node_modules/astro/dist/core/csp/config';
+import { _$, _$All } from './querys';
 
 export const scrollSpyNav = () => {
-  const navLinks = _$All('.nav-link');
+  const navLinks = _$All('.nav-link, .mobile-nav-link');
   const sections = _$All('section');
 
-  const spyObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const id = entry.target.getAttribute('id');
-        const activeLink = _$(`.nav-link[href="#${id}"]`);
+  const spyObserver = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const id = entry.target.getAttribute('id');
+          const activeLink = _$All(`.nav-link[href="#${id}"], .mobile-nav-link[href="#${id}"]`);
 
-        navLinks.forEach(link => link.classList.remove('nav-link-active'));
+          navLinks.forEach(link => {
+            link.classList.remove('nav-link-active');
+          });
 
-        if (activeLink) {
-          activeLink.classList.add('nav-link-active');
+          if (activeLink.length > 0) {
+            activeLink.forEach(link => {
+              link.classList.add('nav-link-active');
+            });
+          }
         }
-      }
-    });
-  }, {
-      rootMargin: '-40% 0px -50% 0px' // Se activa cuando la sección está en el centro de la pantalla
-  });
+      });
+    },
+    {
+      rootMargin: '-40% 0px -50% 0px', // Se activa cuando la sección está en el centro de la pantalla
+    }
+  );
 
   sections.forEach(section => {
-      spyObserver.observe(section);
+    spyObserver.observe(section);
   });
-}
+};
